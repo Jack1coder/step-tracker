0787ff861777be10b56a3bfa5cb8c83e505c3757
Jack1coder <127622048+Jack1coder@users.noreply.github.com>
2023-03-11 18:01:44 +0000

    change power settings on b new reward icon

--- main.ts
+++ main.ts
@@ -4,10 +4,11 @@ input.onGesture(Gesture.Shake, function () {
 })
 let steps = 0
 let set_steps = 0
+power.fullPowerOn(FullPowerSource.B)
 basic.forever(function () {
     let steps_1000 = 0
     basic.showNumber(steps)
     if (steps_1000) {
-        basic.showString("\"well done nearly there\"")
+        basic.showString(Kitronik_VIEWTEXT32.displayEmjoicon(Kitronik_VIEWTEXT32.Emoticon.Happy))
     }
 })
